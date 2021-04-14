a460984cba5964ae0d935ec13e98ceddaa6b5798
HR Williams <halrubinwilliams@gmail.com>
2021-04-13 19:50:47 -0700

    Loop through each item in myGroceryList

--- GroceryList.cs
+++ GroceryList.cs
@@ -4,5 +4,12 @@ class GroceryList
   static void Main()
   {
     string[] myGroceryList = {"eggs", "milk", "bread", "bananas", "cereal", "rice", "yogurt"};
+
+    Console.WriteLine("My grocery list:");
+
+    foreach (string groceryItem in myGroceryList)
+    {
+      Console.WriteLine(groceryItem);
+    }
   }
 }
